efdbfe9ae5a7d20507e5bbe9bdfade2b2ecb474b
Luke <31931505+l-schultz3@users.noreply.github.com>
2018-11-06 14:08:30 -0700

    Add files via upload

--- TicTacToe_OrderedGame.py
+++ TicTacToe_OrderedGame.py
@@ -6,6 +6,14 @@ allWinningGames = []
 foundAMatch = False
 numberOfAttempts = 0
 
+def writeGameToFile(currentGame):
+    print(currentGame)
+    for z in range(9):
+        if (z < 8):
+            gameFile.write(str(currentGame[z][0]))
+        else:
+            gameFile.write(str(currentGame[z][0]) + "\n")
+
 def generateGame():
     currentGameMatches = False
     currentGame = [[5, 0], [5, 0], [5, 0], [5, 0], [5, 0], [5, 0], [5, 0], [5, 0], [5, 0]]
@@ -28,22 +36,63 @@ def generateGame():
                     if (currentGame[c][0] == 5):
                         currentGame[c][0] = 1
                         currentGame[c][1] = 3
-                        print(currentGame)
-                        gameFile.write("\n")
-                        for i in range(9):
-                            if (i == 0):
-                                gameFile.write("{" + str(currentGame[i][0]) + ", ")
-                            elif (i < 8):
-                                gameFile.write(str(currentGame[i][0]) + ", ")
-                            else:
-                                gameFile.write(str(currentGame[i][0]) + "}, ")
+
+                        for d in range(9):
+                            cleanGame(4, currentGame)
+                            if (currentGame[d][0] == 5):
+                                currentGame[d][0] = 0
+                                currentGame[d][1] = 4
+
+                                for e in range(9):
+                                    cleanGame(5, currentGame)
+                                    if (currentGame[e][0] == 5):
+                                        currentGame[e][0] = 1
+                                        currentGame[e][1] = 5
+
+                                        if (check.runCheckWin == "X" or check.runCheckWin == "O"):
+                                            writeGameToFile(currentGame)
+                                        else:
+                                            for f in range(9):
+                                                cleanGame(6, currentGame)
+                                                if (currentGame[f][0] == 5):
+                                                    currentGame[f][0] = 0
+                                                    currentGame[f][1] = 6
+
+                                                    if (check.runCheckWin == "X" or check.runCheckWin == "O"):
+                                                        writeGameToFile(currentGame)
+                                                    else:
+                                                        for g in range(9):
+                                                            cleanGame(7, currentGame)
+                                                            if (currentGame[g][0] == 5):
+                                                                currentGame[g][0] = 1
+                                                                currentGame[g][1] = 7
+
+                                                                if (check.runCheckWin == "X" or check.runCheckWin == "O"):
+                                                                    writeGameToFile(currentGame)
+                                                                else:
+                                                                    for h in range(9):
+                                                                        cleanGame(8, currentGame)
+                                                                        if (currentGame[h][0] == 5):
+                                                                            currentGame[h][0] = 0
+                                                                            currentGame[h][1] = 8
+
+                                                                            if (check.runCheckWin == "X" or check.runCheckWin == "O"):
+                                                                                writeGameToFile(currentGame)
+                                                                            else:
+                                                                                for i in range(9):
+                                                                                    cleanGame(9, currentGame)
+                                                                                    if (currentGame[i][0] == 5):
+                                                                                        currentGame[i][0] = 1
+                                                                                        currentGame[i][1] = 9
+
+                                                                                        writeGameToFile(currentGame)
 
 def cleanGame(move, currentGame):
     for j in range(9):
         if (currentGame[j][1] >= move):
             currentGame[j] = [5, 0]
 
-gameFile = open("gameFile.txt", "w")
+gameFile = open("showGames/gameFile.txt", "w")
 
 generateGame()
 
