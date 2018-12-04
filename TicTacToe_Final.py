b1659c3c58422e5e8c4299f23a11238518ce4c76
Luke <31931505+l-schultz3@users.noreply.github.com>
2018-12-03 20:03:04 -0700

    Add files via upload

--- TicTacToe_Final.py
+++ TicTacToe_Final.py
@@ -13,6 +13,7 @@ def checkWin(gameToCheck):
     elif (gameToCheck[2][0] == gameToCheck[5][0] == gameToCheck[8][0] and gameToCheck[2][0] != None): return "right vertical"
     elif (gameToCheck[0][0] == gameToCheck[4][0] == gameToCheck[8][0] and gameToCheck[0][0] != None): return "backward diagonal"
     elif (gameToCheck[2][0] == gameToCheck[4][0] == gameToCheck[6][0] and gameToCheck[2][0] != None): return "forward diagonal"
+    else: return False
 
 def cleanGame(gameToClean, moveNumber):
     for j in range(9):
@@ -20,7 +21,8 @@ def cleanGame(gameToClean, moveNumber):
             gameToClean[j] = [None, 0]
 
 def generateGames():
-    global allGames, winningGames
+    global allGames
+    global winningGames
     currentGame= [[None, 0], [None, 0], [None, 0], [None, 0], [None, 0], [None, 0], [None, 0], [None, 0], [None, 0]]
 
     for a in range(9):
@@ -28,50 +30,62 @@ def generateGames():
         currentGame[a] = [1, 1]
         for b in range(9):
             cleanGame(currentGame, 2)
-            currentGame[b] = [0, 2]
-            for c in range(9):
-                cleanGame(currentGame, 3)
-                currentGame[c] = [1, 3]
-                for d in range(9):
-                    cleanGame(currentGame, 4)
-                    currentGame[d] = [0, 4]
-                    for e in range(9):
-                        cleanGame(currentGame, 5)
-                        currentGame[e] = [1, 5]
-                        if (checkWin(currentGame) != None):
-                            allGames.append(currentGame)
-                            winningGames.append(currentGame)
-                        else:
-                            for f in range(9):
-                                cleanGame(currentGame, 6)
-                                currentGame[f] = [0, 6]
-                                if (checkWin(currentGame) != None):
-                                    allGames.append(currentGame)
-                                    winningGames.append(currentGame)
-                                else:
-                                    for g in range(9):
-                                        cleanGame(currentGame, 7)
-                                        currentGame[g] = [1, 7]
-                                        if (checkWin(currentGame) != None):
-                                            allGames.append(currentGame)
+            if (currentGame[b] == [None, 0]):
+                currentGame[b] = [0, 2]
+                for c in range(9):
+                    cleanGame(currentGame, 3)
+                    if (currentGame[c] == [None, 0]):
+                        currentGame[c] = [1, 3]
+                        for d in range(9):
+                            cleanGame(currentGame, 4)
+                            if (currentGame[b] == [None, 0]):
+                                currentGame[d] = [0, 4]
+                                for e in range(9):
+                                    cleanGame(currentGame, 5)
+                                    if (currentGame[e] == [None, 0]):
+                                        currentGame[e] = [1, 5]
+                                        print(currentGame)
+                                        if (checkWin(currentGame) != False):
+                                            print(True)
+#                                        allGames.append(currentGame)
                                             winningGames.append(currentGame)
                                         else:
-                                            for h in range(9):
-                                                cleanGame(currentGame, 8)
-                                                currentGame[h] = [0, 8]
-                                                if (checkWin(currentGame) != None):
-                                                    allGames.append(currentGame)
-                                                    winningGames.append(currentGame)
-                                                else:
-                                                    for i in range(9):
-                                                        cleanGame(currentGame, 9)
-                                                        currentGame[i] = [1, 9]
-                                                        allGames.append(currentGame)
-                                                        #print(len(allGames))
-                                                        if (checkWin(currentGame) != None):
-                                                            winningGames.append(currentGame)
-
-
+                                            for f in range(9):
+                                                cleanGame(currentGame, 6)
+                                                if (currentGame[f] == [None, 0]):
+                                                    currentGame[f] = [0, 6]
+                                                    if (checkWin(currentGame) != False):
+#                                                    allGames.append(currentGame)
+                                                        winningGames.append(currentGame)
+                                                    else:
+                                                        for g in range(9):
+                                                            cleanGame(currentGame, 7)
+                                                            if (currentGame[g] == [None, 0]):
+                                                                currentGame[g] = [1, 7]
+                                                                if (checkWin(currentGame) != False):
+#                                                                allGames.append(currentGame)
+                                                                    winningGames.append(currentGame)
+                                                                else:
+                                                                    for h in range(9):
+                                                                        cleanGame(currentGame, 8)
+                                                                        if (currentGame[h] == [None, 0]):
+                                                                            currentGame[h] = [0, 8]
+                                                                            if (checkWin(currentGame) != False):
+#                                                                            allGames.append(currentGame)
+                                                                                winningGames.append(currentGame)
+                                                                            else:
+                                                                                for i in range(9):
+                                                                                    cleanGame(currentGame, 9)
+                                                                                    if (currentGame[i] == [None, 0]):
+                                                                                        currentGame[i] = [1, 9]
+                                                                                        print(currentGame)
+#                                                                                    allGames.append(currentGame)
+                                                                                    #print(len(allGames))
+                                                                                        if (checkWin(currentGame) != False):
+                                                                                            winningGames.append(currentGame)
+                                                                                            print(currentGame)
+                                                                                        else:
+                                                                                            print(currentGame)
 generateGames()
 
 print(len(allGames))
